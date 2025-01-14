3b9036cb02839257d3cb14a9fd7534bb9e8cb9d2
Your Name <oghenetegafrancis@gmail.com>
2025-01-14 12:11:23 +0100

    worked on login and signup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect }  from 'react'
+import appwriteService from "../appwrite/config"
+import Container from '../components/container/Container'
+import PostCard from "../components/PostCard"
 
 function Home() {
+  const [posts, setPosts] = useState()
+
+  useEffect(() => {
+    appwriteService.getPosts([]).then((post) => {
+      if (posts) {
+        setPosts(post.documents)
+      }
+    })
+  }, [])
+
+  if (posts.length === 0) {
+    return (
+      <div className='w-full py-8'>
+      <Container>
+        <div className="flex flex-wrap">
+          <h1> Login to see posts</h1>
+        </div>
+      </Container>
+    </div>
+    )
+  }
+
   return (
-    <div>
-      Home
+    <div className='w-full py-8'>
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div className="p-2 w-1/4" key={post.$id}> 
+              <PostCard {...post} />
+            </div>
+          ))}
+        </div>
+      </Container>
     </div>
   )
 }
