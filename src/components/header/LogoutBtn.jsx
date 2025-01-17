c1f1037960f1ecb245216c7d66726a2002e4ae8e
Your Name <oghenetegafrancis@gmail.com>
2025-01-17 16:20:45 +0100

    Debugged LogoutBtn

--- src/components/header/LogoutBtn.jsx
+++ src/components/header/LogoutBtn.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import authService from "../../appwrite/auth.js"
-import { logout } from "../../store/authSlice.js"
+import { clearUser } from "../../store/authSlice.js"
 
 function LogoutBtn() {
     const dispatch = useDispatch()
 
     const LogoutHandler = () => {
-        authService.logout().then(() => {
-            dispatch(logout());
+        authService.clearUser().then(() => {
+            dispatch(clearUser());
         })
     }
 
